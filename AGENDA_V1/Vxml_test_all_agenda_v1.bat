fbd26d997674fd9b36f1e554b243f860fe1bec51
Bebel19 <belgnaoui.othman@gmail.com>
2025-10-19 19:17:11 +0200

    fixex quit grammar

--- AGENDA_V1/Vxml_test_all_agenda_v1.bat
+++ AGENDA_V1/Vxml_test_all_agenda_v1.bat
@@ -8,7 +8,7 @@ rem ot_grammar_tester.exe  ...grammaire_num_ab_v2.grxml
 rem ot_grammar_tester.exe  ...grammaire_dates_v3.grxml
 rem ot_grammar_tester.exe  ...grammaire_act_lang_confirmation_v2.grxml
 
-rem ot_vxml_interpreter.exe    ...gestion_agenda_v0.vxml
+rem ot_vxml_interpreter.exe    ...gestion_agenda_v1.vxml
 
 
 
